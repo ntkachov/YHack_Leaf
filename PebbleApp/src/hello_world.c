7f51206fcce5e2598e10a052740375f683811eb6
Marco Chiang <marcochiangtw@gmail.com>
2013-11-09 11:45:48 -0500

    updated app

--- PebbleApp/src/hello_world.c
+++ PebbleApp/src/hello_world.c
@@ -1,7 +1,23 @@
 #include <pebble.h>
+#include "acceleromonitor.h"
+
+#define HISTORY_MAX 144
+#define MAX_ACCEL 4000
+//#define LOG_PRINT_ACCEL 25
+//#define TARGET_PEAKS = 4; 
+
 
 static Window *window;
 static TextLayer *text_layer;
+
+//acceleromter fields
+static AppTimer *timer;
+
+int timer_frequency = 100;
+bool running = false;
+static int sample_window = 30;
+static int last_x = 0;
+static AccelData history[HISTORY_MAX];
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -14,6 +30,12 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
         return;
     }
     app_message_outbox_send();
+}
+
+static void set_timer() {
+  if (running){
+       timer = app_timer_register(timer_frequency, timer_callback, NULL);
+    }
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -47,10 +69,85 @@ static void window_unload(Window *window) {
 static void in_received_handler(DictionaryIterator *iter, void *context){
 }
 
+static void timer_callback() {
+  if (!running) return;
+
+  AccelData accel = {0, 0, 0};
+
+  accel_service_peek(&accel);
+  
+  history[last_x].x = accel.x;
+  history[last_x].y = accel.y;
+  history[last_x].z = accel.z;
+
+  if (last_x >= sample_window){
+    get_variance();
+  }
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%i, %i, %i", accel.x, accel.y, accel.z); 
+  last_x++;
+  if (last_x >= HISTORY_MAX) last_x = 0;
+  //layer_mark_dirty(graph_layer);
+
+  set_timer();
+}
+
+
+static void get_variance(){
+  int16_t recent_max = 0;
+  int16_t recent_min = 0;
+  
+  //find recent high and low
+  for (int k = last_x - sample_window; k <= last_x; k++){
+    if (history[k].y >= recent_max) recent_max = history[k].y;
+    else if (history[k].y <= recent_min) recent_min = history[k].y;
+  }
+  
+  //measure variance
+  int16_t difference = recent_max - recent_min;
+  if (difference > 2000){
+    DictionaryIterator *iter;
+
+    if (app_message_outbox_begin(&iter) != APP_MSG_OK) {
+        return;
+    }
+    if (dict_write_uint8(iter, 4, 5) != DICT_OK) {
+        return;
+    }
+    app_message_outbox_send();
+    //throw a handshake
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Handshake: %i", difference);
+  }
+}
+
 static void app_message_init(void){
     app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
     app_message_open(64, 16);
     app_message_register_inbox_received(in_received_handler);
+}
+
+static void handle_accel(AccelData *accel_data, uint32_t num_samples) {
+  // nothing
+}
+
+static void start_accel(){
+  running = !running;
+    if (running) {
+    // should process events - 5 times a second
+      accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+      accel_data_service_subscribe(0, handle_accel);
+      layer_set_hidden((Layer *)text_layer, true);
+      //layer_set_hidden((Layer *)speed_up_layer, true);
+      //layer_set_hidden((Layer *)speed_down_layer, true);
+    }
+    else {
+      layer_set_hidden((Layer *)text_layer, false);
+      //layer_set_hidden((Layer *)speed_up_layer, false);
+      //layer_set_hidden((Layer *)speed_down_layer, false);
+      accel_data_service_unsubscribe();
+    }
+
+    set_timer();
 }
 
 static void init(void) {
@@ -63,9 +160,29 @@ static void init(void) {
   const bool animated = true;
   window_stack_push(window, animated); 
   app_message_init();
+
+  //plug dummy values into history array, start timer
+  for (int i=0; i<HISTORY_MAX; ++i) {
+    AccelData pt = {0, 0, 0};
+    history[i] = pt;
+  }
+
+    DictionaryIterator *iter;
+
+      if (app_message_outbox_begin(&iter) != APP_MSG_OK) {
+          return;
+    }
+      if (dict_write_uint8(iter, 4, 5) != DICT_OK) {
+                      return;
+    }
+      app_message_outbox_send();
+
+    start_accel();
+//  set_timer();
 }
 
 static void deinit(void) {
+  accel_data_service_unsubscribe();
   window_destroy(window);
 }
 
