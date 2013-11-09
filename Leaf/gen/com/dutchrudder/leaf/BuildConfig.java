dbc8beca1117394786ffd752dc7d643e7e893aed
Nikita Tkachov <ntkachov@gmail.com>
2013-11-09 03:29:57 -0500

    Adding android

---
+++ Leaf/gen/com/dutchrudder/leaf/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dutchrudder.leaf;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Leaf/src/com/dutchrudder/leaf/PebbleReceiver.java
@@ -0,0 +1,23 @@
+package com.dutchrudder.leaf;
+
+import java.util.UUID;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.getpebble.android.kit.PebbleKit.PebbleDataReceiver;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+public class PebbleReceiver extends PebbleDataReceiver{
+
+	protected PebbleReceiver(UUID subscribedUuid) {
+		super(subscribedUuid);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void receiveData(Context context, int transactionId, PebbleDictionary data) {
+		Log.d("Leaf", "GOT DATA FROM THE PEBBLE");
+	}
+
+}
